85ef100dc09e34d7d8e0f6da3da37f2ff5ff2083
kayhide <kayhide@gmail.com>
2015-09-22 02:35:04 +0200

    create MotionBlender.analyze.

--- lib/motion_blender.rb
+++ lib/motion_blender.rb
@@ -2,5 +2,24 @@ require 'motion_blender/version'
 require 'motion_blender/analyzer'
 
 module MotionBlender
-  # Your code goes here...
+  module_function
+
+  def analyze
+    Motion::Project::App.setup do |app|
+      analyzer = Analyzer.new
+      app.files.flatten.each do |file|
+        analyzer.analyze file
+      end
+
+      if analyzer.files.any?
+        app.exclude_from_detect_dependencies += [ext_file, *analyzer.files]
+        app.files.unshift [ext_file, *(analyzer.files - app.files)]
+        app.files_dependencies analyzer.dependencies
+      end
+    end
+  end
+
+  def ext_file
+    File.expand_path('../../motion/ext.rb', __FILE__)
+  end
 end
